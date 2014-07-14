f0727976cbfd46d9285695bf9e71b04ca7a299b2
Thomas Kirsten <tho_ki@gmx.de>
2014-07-14 15:46:09 +0200

    Better 3D map::sees function
    Monsters will see you on different z-levels now (corrected map::sees)

--- src/map.cpp
+++ src/map.cpp
@@ -4689,103 +4689,131 @@ bool map::sees( const point F, const point T, const int range, int &bresenham_sl
     return sees( F.x, F.y, T.x, T.y, range, bresenham_slope );
 }
 
+bool blocks_z( const map &m, int &last_z, const tripoint &pnt )
+{
+    if( pnt.z < last_z ) {
+        if( m.blocks_vertical_view_up( pnt ) ) {
+            return true;
+        }
+        last_z = pnt.z;
+    } else if( pnt.z > last_z ) {
+        if( m.blocks_vertical_view_down( pnt ) ) {
+            return true;
+        }
+        last_z = pnt.z;
+    }
+    return false;
+}
+
+bool blocks( const map &m, int &last_z, const tripoint &pnt )
+{
+    if( blocks_z( m, last_z, pnt ) ) {
+        return true;
+    } else if( m.light_transparency( pnt ) <= LIGHT_TRANSPARENCY_SOLID ) {
+        return true;
+    }
+    return false;
+}
+
 bool map::sees( const tripoint F, const tripoint T, const int range, int &bresenham_slope ) const
 {
     if( T.z == F.z ) {
     return sees( F.x, F.y, T.x, T.y, range, bresenham_slope );
     }
+    bresenham_slope = 0;
+    // both points are valid, therefor all points between are valid and we don't
+    // have to check that during the loop.
+    if( !inbounds( F ) || !inbounds( T ) ) {
+        return false;
+    }
     const int dx = T.x - F.x;
     const int dy = T.y - F.y;
     const int dz = T.z - F.z;
-    const int ax = abs(dx) * 2;
-    const int ay = abs(dy) * 2;
-    const int az = abs(dz) * 2;
-    const int sx = SGN(dx);
-    const int sy = SGN(dy);
-    const int sz = SGN(dz);
-    tripoint pnt;
-    int t = 0;
-    int tz;
-    int st;
+    if( range >= 0 ) {
+        if( abs( dx ) > range || abs( dy ) > range || abs( dz ) > range ) {
+            return false;
+                }
+                    }
+    tripoint pnt = F;
+    const int x_inc = (dx < 0) ? -1 : 1;
+    const int l = abs(dx);
+    const int y_inc = (dy < 0) ? -1 : 1;
+    const int m = abs(dy);
+    const int z_inc = (dz < 0) ? -1 : 1;
+    const int n = abs(dz);
+    const int dx2 = l << 1;
+    const int dy2 = m << 1;
+    const int dz2 = n << 1;
 
-    if (range >= 0 && range < rl_dist(F, T) ) {
-        return false; // Out of range!
-    }
-    if (ax > ay) { // Mostly-horizontal line
-        st = SGN(ay - (ax / 2));
-        // Doing it "backwards" prioritizes straight lines before diagonal.
-        // T.his will help avoid creating a string of zombies behind you and will
-        // promote "mobbing" behavior (zombies surround you to beat on you)
-        for (bresenham_slope = abs(ay - (ax / 2)) * 2 + 1; bresenham_slope >= -1; bresenham_slope--) {
-            t = bresenham_slope * st;
-            tz = 0;
-            pnt = F;
-            do {
-                if (t > 0) {
-                    pnt.y += sy;
-                    t -= ax;
-                }
-                if (tz > 0) {
-                    bool ok;
-                    if (sz < 0) {
-                        ok = blocks_vertical_view_down(pnt);
-                    } else {
-                        ok = blocks_vertical_view_up(pnt);
+    // Note: in-loop checks for crossing z-level *and* transparency, but last item checks
+    // only z-level crossing because the final square is visible even when it's opaque (like a wall)
+    if ((l >= m) && (l >= n)) {
+        int err_1 = dy2 - l;
+        int err_2 = dz2 - l;
+        int last_z = pnt.z;
+        for (int i = 0; i < l; i++) {
+            if( blocks( *this, last_z, pnt ) ) {
+                return false;
                     }
-                    if (tz == 1 && !ok) {
-                        break;
-                    } else if (ok) {
-                        pnt.z += sz;
-                        tz -= az;
-                    }
-                }
-                pnt.x += sx;
-                t += ay;
-                tz += ay;
-                if (pnt == T) {
-                    bresenham_slope *= st;
-                    return true;
-                }
-            } while( inbounds( pnt ) && light_transparency( pnt ) > LIGHT_TRANSPARENCY_SOLID );
+            if (err_1 > 0) {
+                pnt.y += y_inc;
+                err_1 -= dx2;
+            }
+            if (err_2 > 0) {
+                pnt.z += z_inc;
+                err_2 -= dx2;
+            }
+            err_1 += dy2;
+            err_2 += dz2;
+            pnt.x += x_inc;
         }
-        return false;
-    } else { // Same as above, for mostly-vertical lines
-        st = SGN(ax - (ay / 2));
-        for (bresenham_slope = abs(ax - (bresenham_slope / 2 )) * 2 + 1; bresenham_slope >= -1; bresenham_slope--) {
-            t = bresenham_slope * st;
-            tz = 0;
-            pnt = F;
-            do {
-                if (t > 0) {
-                    pnt.x += sx;
-                    t -= ay;
+        bresenham_slope = err_1 * SGN(dx2 - (dy2 >> 1));
+        return !blocks_z( *this, last_z, pnt );
+    } else if ((m >= l) && (m >= n)) {
+        int err_1 = dx2 - m;
+        int err_2 = dz2 - m;
+        int last_z = pnt.z;
+        for (int i = 0; i < m; i++) {
+            if( blocks( *this, last_z, pnt ) ) {
+                return false;
                 }
-                if (tz > 0) {
-                    bool ok;
-                    if (sz < 0) {
-                        ok = blocks_vertical_view_down(pnt);
-                    } else {
-                        ok = blocks_vertical_view_up(pnt);
-                    }
-                    if (tz == 1 && !ok) {
-                        break;
-                    } else if (ok) {
-                        pnt.z += sz;
-                        tz -= az;
-                    }
+            if (err_1 > 0) {
+                pnt.x += x_inc;
+                err_1 -= dy2;
                 }
-                pnt.y += sy;
-                t += ax;
-                tz += ax;
-                if (pnt == T) {
-                    bresenham_slope *= st;
-                    return true;
-                }
-            } while( inbounds( pnt ) && light_transparency( pnt ) > LIGHT_TRANSPARENCY_SOLID );
+            if (err_2 > 0) {
+                pnt.z += z_inc;
+                err_2 -= dy2;
         }
-        return false;
+            err_1 += dx2;
+            err_2 += dz2;
+            pnt.y += y_inc;
+                }
+        bresenham_slope = err_1 * SGN(dx2 - (dy2 >> 1));
+        return !blocks_z( *this, last_z, pnt );
+                    } else {
+        int err_1 = dy2 - n;
+        int err_2 = dx2 - n;
+        int last_z = pnt.z;
+        for (int i = 0; i < n; i++) {
+            if( blocks( *this, last_z, pnt ) ) {
+                return false;
+                    }
+            if (err_1 > 0) {
+                pnt.y += y_inc;
+                err_1 -= dz2;
+                }
+            if (err_2 > 0) {
+                pnt.x += x_inc;
+                err_2 -= dz2;
+                }
+            err_1 += dy2;
+            err_2 += dx2;
+            pnt.z += z_inc;
+        }
+        bresenham_slope = err_1 * SGN(dx2 - (dy2 >> 1));
+        return !blocks_z( *this, last_z, pnt );
     }
-    return false; // Shouldn't ever be reached, but there it is.
 }
 
 /*
