7dc517db757ef23658760f36f8b2c05d89fa17b6
BevapDin <tho_ki@gmx.de>
2015-11-09 05:02:36 +0100

    Put the documentation for WINDOW_PTR into a group.
    This avoids having to document delwin_functor and WINDOW_PTR separately and makes clear they belong together.

--- src/output.h
+++ src/output.h
@@ -81,24 +81,29 @@ extern int FULL_SCREEN_HEIGHT; // height of "full screen" popups
 extern int OVERMAP_WINDOW_WIDTH; // width of overmap window
 extern int OVERMAP_WINDOW_HEIGHT; // height of overmap window
 
-struct delwin_functor {
-    void operator()( WINDOW *w ) const;
-};
 /**
+ * @name WINDOW pointer wrappers
+ *
  * A Wrapper around the WINDOW pointer, it automatically deletes the
- * window (see delwin_functor) when the variable gets out of scope.
- * This includes calling werase, wrefresh and delwin.
+ * window (via @ref delwin_functor) when the variable gets out of scope.
+ * This includes calling @ref werase, @ref wrefresh and @ref delwin.
+ *
  * Usage:
  * 1. Acquire a WINDOW pointer via @ref newwin like normal, store it in a pointer variable.
  * 2. Create a variable of type WINDOW_PTR *on the stack*, initialize it with the pointer from 1.
- * 3. Do the usual stuff with window, print, update, etc. but do *not* call delwin on it.
+ * 3. Do the usual stuff with window, print, update, etc. but do *not* call @ref delwin on it.
  * 4. When the function is left, the WINDOW_PTR variable is destroyed, and its destructor is called,
  *    it calls werase, wrefresh and most importantly delwin to free the memory associated wit the pointer.
  * To trigger the delwin call earlier call some_window_ptr.reset().
  * To prevent the delwin call when the function is left (because the window is already deleted or, it should
  * not be deleted), call some_window_ptr.release().
  */
-typedef std::unique_ptr<WINDOW, delwin_functor> WINDOW_PTR;
+/*@{*/
+struct delwin_functor {
+    void operator()( WINDOW *w ) const;
+};
+using WINDOW_PTR = std::unique_ptr<WINDOW, delwin_functor> ;
+/*@}*/
 
 /**
  * @defgroup game_message_type Message types
