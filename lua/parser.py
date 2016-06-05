bcfc58e1004dcb340dca4b40aa93e9aedb2ba3fe
BevapDin <tho_ki@gmx.de>
2016-06-05 19:41:44 +0200

    Add generic code to export any kind of std::vector to Lua.

--- lua/parser.py
+++ lua/parser.py
@@ -408,6 +408,8 @@ class Parser:
     def register_generic(self, t):
         res = self.register_container('list', t)
         if res: return res
+        res = self.register_container('vector', t)
+        if res: return res
 
         return None
 
