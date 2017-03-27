abc70b5b5a446ec82eef266b36efe257925124c4
BevapDin <Tho_Ki@gmx.de>
2017-03-27 18:02:14 +0200

    Increase batch crafting number.
    # Conflicts:
    #	src/crafting.cpp

--- src/crafting_gui.cpp
+++ src/crafting_gui.cpp
@@ -235,7 +235,7 @@ const recipe *select_crafting_recipe( int &batch_size )
 
             if( batch ) {
                 current.clear();
-                for( int i = 1; i <= 20; i++ ) {
+                for( int i = 1; i <= 64; i++ ) {
                     current.push_back( chosen );
                     available.push_back( chosen->requirements().can_make_with_inventory( crafting_inv, i ) );
                 }
