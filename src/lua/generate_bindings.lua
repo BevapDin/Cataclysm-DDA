22d9bfb70ed879f153d5ba7893312e4e138fe533
BevapDin <Tho_Ki@gmx.de>
2017-04-20 11:53:36 +0200

    generate_bindings.lua: remove redundant return value of getter functions
    They *always* return 1, the caller (which is only one place) can do that instead.

--- src/lua/generate_bindings.lua
+++ src/lua/generate_bindings.lua
@@ -132,14 +132,14 @@ end
 -- Generates a getter function for a specific class and member variable.
 function generate_getter(class_name, member_name, member_type, cpp_name, static)
     local function_name = "get_" .. cpp_ident(class_name) .. "_" .. member_name
-    local text = "static int "..function_name.."(lua_State *L) {"..br
+    local text = "static void "..function_name.."(lua_State *L) {"..br
 
     if static then
-        text = text .. tab .. 'return ' .. push_lua_value(class_name .. '::' .. cpp_name, member_type .. "&")..';'..br
+        text = text .. tab .. push_lua_value(class_name .. '::' .. cpp_name, member_type .. "&")..';'..br
     else
         text = text .. tab .. load_instance(class_name)..br
         -- adding the "&" to the type, so push_lua_value knows it's a reference.
-        text = text .. tab .. 'return ' .. push_lua_value("instance."..cpp_name, member_type .. "&")..';'..br
+        text = text .. tab .. push_lua_value("instance."..cpp_name, member_type .. "&")..';'..br
     end
     text = text .. "}" .. br
 
