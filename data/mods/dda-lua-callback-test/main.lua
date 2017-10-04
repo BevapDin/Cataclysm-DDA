49105781989a01bb9aa9b05171cdb1a6863a0dc4
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2017-10-04 04:06:54 +0300

    Updated callback test mod

--- data/mods/dda-lua-callback-test/main.lua
+++ data/mods/dda-lua-callback-test/main.lua
@@ -1,7 +1,7 @@
 local MOD = {
 
   id = "dda-lua-callback-test",
-  version = "2017-09-01"
+  version = "2017-10-04"
 
 }
 
@@ -9,119 +9,125 @@ mods[MOD.id] = MOD
 
 function MOD.on_game_loaded() 
 
-  MOD.Test("on_game_loaded")
+  MOD.PrintDebugInfo("on_game_loaded")
 
 end
 
 function MOD.on_savegame_loaded()
 
-  MOD.Test("on_savegame_loaded")
+  MOD.PrintDebugInfo("on_savegame_loaded")
 
 end
 
 function MOD.on_stat_change()
 
-  MOD.Test("on_stat_change")
+  MOD.PrintDebugInfo("on_stat_change")
 
 end
 
 function MOD.on_skill_increased()
 
-  MOD.Test("on_skill_increased")
+  MOD.PrintDebugInfo("on_skill_increased")
 
 end
 
 function MOD.on_turn_passed()
 
-  MOD.Test("on_turn_passed")
+  MOD.PrintDebugInfo("on_turn_passed")
 
 end
 
 function MOD.on_second_passed()
 
-  MOD.Test("on_second_passed")
+  MOD.PrintDebugInfo("on_second_passed")
 
 end
 
 function MOD.on_minute_passed()
 
-  MOD.Test("on_minute_passed")
+  MOD.PrintDebugInfo("on_minute_passed")
 
 end
 
 function MOD.on_hour_passed()
 
-  MOD.Test("on_hour_passed")
+  MOD.PrintDebugInfo("on_hour_passed")
 
 end
 
 function MOD.on_day_passed()
 
-  MOD.Test("on_day_passed")
+  MOD.PrintDebugInfo("on_day_passed")
 
 end
 
 function MOD.on_year_passed()
 
-  MOD.Test("on_year_passed")
+  MOD.PrintDebugInfo("on_year_passed")
 
 end
 
 function MOD.on_mutation_gain()
 
-  MOD.Test("on_mutation_gain")
+  MOD.PrintDebugInfo("on_mutation_gain")
 
 end
 
 function MOD.on_mutation_loss()
 
-  MOD.Test("on_mutation_loss")
+  MOD.PrintDebugInfo("on_mutation_loss")
 
 end
 
 function MOD.on_stat_change()
 
-  MOD.Test("on_stat_change")
+  MOD.PrintDebugInfo("on_stat_change")
 
 end
 
 function MOD.on_item_wear()
 
-  MOD.Test("on_item_wear")
+  MOD.PrintDebugInfo("on_item_wear")
 
 end
 
 function MOD.on_item_takeoff()
 
-  MOD.Test("on_item_takeoff")
+  MOD.PrintDebugInfo("on_item_takeoff")
 
 end
 
 function MOD.on_effect_int_changes()
 
-  MOD.Test("on_effect_int_change")
+  MOD.PrintDebugInfo("on_effect_int_change")
 
 end
 
 function MOD.on_mission_assignment()
 
-  MOD.Test("on_mission_assignment")
+  MOD.PrintDebugInfo("on_mission_assignment")
 
 end
 
 function MOD.on_mission_finished()
 
-  MOD.Test("on_mission_finished")
+  MOD.PrintDebugInfo("on_mission_finished")
 
 end
 
-function MOD.on_mapgen_finished() 
+function MOD.on_json_mapgen_finished() 
 
-  MOD.Test("on_mapgen_finished")
+  MOD.PrintDebugInfo("on_json_mapgen_finished")
 
 end
 
-MOD.Test = function(s)
+function MOD.on_lua_mapgen_finished() 
+
+  MOD.PrintDebugInfo("on_lua_mapgen_finished")
+
+end
+
+MOD.PrintDebugInfo = function(s)
 
   if (game.add_msg) then
     game.add_msg ("<color_cyan>     function: </color><color_ltcyan>"..tostring(s).."</color>")
