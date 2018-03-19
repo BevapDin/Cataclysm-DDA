45d26ec591f7ed88da67ef5cd8d72b97fc1ec2b0
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2018-03-19 18:17:58 +0300

    Updated mod log location

--- data/mods/dda-lua-test-callback/init.lua
+++ data/mods/dda-lua-test-callback/init.lua
@@ -8,4 +8,4 @@ log = require("./data/mods/dda-lua-test-callback/log")
 
 --[[ Globals initialization ]]--
 
-LOG = log.init("./dda-lua-test-callback.log", DEBUG_ENABLED or false, DEBUG_ENABLED or false)
+LOG = log.init("./config/dda-lua-test-callback.log", DEBUG_ENABLED or false, DEBUG_ENABLED or false)
