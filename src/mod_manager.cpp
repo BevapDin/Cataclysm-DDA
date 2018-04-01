37aff62d41a27b2f6bb35310c135d7f3cec1fea0
BevapDin <Tho_Ki@gmx.de>
2018-04-01 13:12:14 +0200

    Use string concatenation instead of ostringstream

--- src/mod_manager.cpp
+++ src/mod_manager.cpp
@@ -325,13 +325,12 @@ bool mod_manager::copy_mod_contents( const t_mod_list &mods_to_copy,
         }
 
         // create needed directories
-        std::ostringstream cur_mod_dir;
-        cur_mod_dir << output_base_path << "/" << string_format( "mod_%05d", i + 1 );
+        const std::string cur_mod_dir = output_base_path + "/" + string_format( "mod_%05d", i + 1 );
 
         std::queue<std::string> dir_to_make;
-        dir_to_make.push( cur_mod_dir.str() );
+        dir_to_make.push( cur_mod_dir );
         for( auto &input_dir : input_dirs ) {
-            dir_to_make.push( cur_mod_dir.str() + "/" + input_dir.substr( start_index ) );
+            dir_to_make.push( cur_mod_dir + "/" + input_dir.substr( start_index ) );
         }
 
         while( !dir_to_make.empty() ) {
@@ -346,7 +345,7 @@ bool mod_manager::copy_mod_contents( const t_mod_list &mods_to_copy,
         // trim file paths from full length down to just /data forward
         for( auto &input_file : input_files ) {
             std::string output_path = input_file;
-            output_path = cur_mod_dir.str() + output_path.substr( start_index );
+            output_path = cur_mod_dir + output_path.substr( start_index );
             copy_file( input_file, output_path );
         }
     }
