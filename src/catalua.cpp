304f300e5cb1efea9e876efeb4a90ae810f7b207
BevapDin <Tho_Ki@gmx.de>
2018-06-01 18:44:00 +0200

    Move class template LuaValueOrReference into separate header

--- src/catalua.cpp
+++ src/catalua.cpp
@@ -147,37 +147,7 @@ bool lua_report_error( lua_State *L, int err, const char *path, bool simple = fa
 #include "lua/reference.h"
 #include "lua/type.h"
 #include "lua/enum.h"
-
-/**
- * Wrapper class to access objects in Lua that are stored as either a pointer or a value.
- * Technically, this class could inherit from both `LuaValue<T>` and `LuaReference<T>`,
- * but that would basically the same code anyway.
- * It behaves like a LuaValue if there is a value on the stack, and like LuaReference is there
- * is a reference on the stack. Functions behave like the functions in a `LuaType`.
- * Note that it does not have a push function because it can not know whether to push a reference
- * or a value (copy). The caller must decide this and must use `LuaValue` or `LuaReference`.
- */
-template<typename T>
-class LuaValueOrReference
-{
-    public:
-        using proxy = typename LuaReference<T>::proxy;
-        static proxy get( lua_State *const L, int const stack_index ) {
-            if( LuaValue<T>::has( L, stack_index ) ) {
-                return proxy{ &LuaValue<T>::get( L, stack_index ) };
-            }
-            return LuaReference<T>::get( L, stack_index );
-        }
-        static void check( lua_State *const L, int const stack_index ) {
-            if( LuaValue<T>::has( L, stack_index ) ) {
-                return;
-            }
-            LuaValue<T *>::check( L, stack_index );
-        }
-        static bool has( lua_State *const L, int const stack_index ) {
-            return LuaValue<T>::has( L, stack_index ) || LuaValue<T *>::has( L, stack_index );
-        }
-};
+#include "lua/value_or_reference.h"
 
 void update_globals( lua_State *L )
 {
