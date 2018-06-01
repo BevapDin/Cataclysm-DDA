9c070ece1b9b349806057adae6840336f6d4086b
BevapDin <Tho_Ki@gmx.de>
2018-06-01 11:26:53 +0200

    Lua: prefix enumeration values with name of the enumeration
    This enables proper support for `enum class` enumerations. (It's backwards compatible with old style enumerations.) Instead of putting the full name of the enumeration value into "lua/class_defintions.lua", the wrapper adds it on its own.

--- lua/class_definitions.lua
+++ lua/class_definitions.lua
@@ -2015,9 +2015,9 @@ enums = {
         "num_hp_parts",
     },
     side = {
-        "side::BOTH",
-        "side::LEFT",
-        "side::RIGHT"
+        "BOTH",
+        "LEFT",
+        "RIGHT"
     },
     phase_id = {
         "PNULL",
