7957f5ae2c701f78f6afbabdfbbf78512a1e7f25
BevapDin <Tho_Ki@gmx.de>
2018-06-03 13:25:38 +0200

    Remove lua_engine::call, use `catalua::call` instead.
    As that function is more generic (can handle parameters and return value).

--- src/lua/console.cpp
+++ src/lua/console.cpp
@@ -1,6 +1,7 @@
 #include "console.h"
 
 #include "lua_engine.h"
+#include "call.h"
 #include "catacharset.h"
 #include "input.h"
 #include "string_input_popup.h"
@@ -107,7 +108,7 @@ void console::run()
 
         std::string input = get_input();
 
-        engine.call( input );
+        catalua::call<void>( engine, input );
 
         read_stream( engine.output_stream, c_white );
         read_stream( engine.error_stream, c_red );
