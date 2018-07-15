bebf917fc438918c4ba9f1b3921539bc4baab56c
BevapDin <Tho_Ki@gmx.de>
2018-07-16 01:20:38 +0200

    inline docu

--- src/lua/call.h
+++ src/lua/call.h
@@ -62,10 +62,7 @@ template<>
 class call_wrapper<void>
 {
     public:
-        template<typename ... Args>
-        static void call( const lua_engine &engine, const std::string &script, Args &&... args ) {
-            push_script( engine, script );
-            const int cnt = push_onto_stack( engine, std::forward<Args>( args )... );
+        static void call( const lua_engine &engine, const int cnt ) {
             call_void_function( engine, cnt );
         }
 };
@@ -74,10 +71,7 @@ template<typename T>
 class call_wrapper
 {
     public:
-        template<typename ... Args>
-        static T call( const lua_engine &engine, const std::string &script, Args &&... args ) {
-            push_script( engine, script );
-            const int cnt = push_onto_stack( engine, std::forward<Args>( args )... );
+        static T call( const lua_engine &engine, const int cnt ) {
             call_non_void_function( engine, cnt );
             return pop_from_stack<T>( engine, -1 );
         }
@@ -85,32 +79,36 @@ class call_wrapper
 } // namespace detail
 
 /**
- * Generic global function to run any arbitrary Lua code.
+ * @brief Generic global function to run any arbitrary Lua code.
  *
- * @tparam value_type The type that is returned by the script. Can be simply `void`.
- * This must be specified explicitly as the compiler can not detect it. Note that only
- * few types are actually supported here. It can be a reference.
- * @throws @ref std::exception if the Lua code does not work out )-; 
- *
- * The interface is designed to *not* require the `lua_engine` to be a complete type,
- * that means one can use the interface while only having a *reference* to a `lua_engine`.
- * 
  * Usage:
  *
 <code>
 item result = catalua::call<item>( *g->lua_engine_ptr, "some lua script that returns an item value", some, more, parameters );
 </code>
+ *
+ * @tparam value_type The type that is returned by the script. Can be simply `void`.
+ * This must be specified explicitly as the compiler can not detect it. Note that only
+ * few types are actually supported here. It can be a reference (but not a `std::reference_wrapper`).
+ * @throws @ref std::exception if the Lua code does not work out )-;
  */
 template<typename value_type, typename ... Args>
 inline value_type call( const lua_engine &engine, const std::string &script, Args &&... args )
 {
-    return detail::call_wrapper<value_type>::call( engine, script, std::forward<Args>( args )... );
+    // The interface is designed to *not* require the `lua_engine` to be a complete type,
+    // that means one can use the interface while only having a *reference* to a `lua_engine`.
+    push_script( engine, script );
+    const int cnt = push_onto_stack( engine, std::forward<Args>( args )... );
+    return detail::call_wrapper<value_type>::call( engine, cnt );
 }
 
 /**
- * Invokes a callback that can be registered by each mod. The callback is invoked for each mod.
+ * Invokes a callback that can be registered by each mod. The callback is invoked
+ * in an arbitrary order for each mod. If no mod has registered the callback, nothing
+ * will happen.
  * The function does *not* return anything as it may result in several calls (for several mods),
  * or in no call at all (no mod has registered the callback).
+ * Besides the fixed return type, it behaves like @ref call.
  * @param name Name of the callback. Should be a simple constant identifier name. Make sure to
  * document the callbacks.
  * @throws No errors from Lua, those are reported a different way.
@@ -119,6 +117,8 @@ inline value_type call( const lua_engine &engine, const std::string &script, Arg
 template<typename ... Args>
 inline void mod_callback( const lua_engine &engine, const char *const name, Args &&... args )
 {
+    // Note: this does not throw as it is used far to often and all over the code.
+    // Requiring the caller to handle errors would cause too much disturbance.
     try {
         push_mod_callback_call( engine );
         // This pushes a reference to a function in Lua onto the stack, that function
