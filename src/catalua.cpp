7f2390de153bccfa54eee605a75d6f101f4edcaf
BevapDin <Tho_Ki@gmx.de>
2018-07-30 11:38:03 +0200

    Lua: remove get_terrain_type function
    One can use the `string_id`/`int_id` class to convert strings/integers
    into `ter_t` references:
    
    ```Lua
    local tername = ter_id(3):obj().name
    local terdesc = ter_str_id("t_dirt"):obj().description
    ```

--- src/catalua.cpp
+++ src/catalua.cpp
@@ -879,11 +879,6 @@ int lua_mapgen(map *m, const oter_id &terrain_type, const mapgendata &, const ti
 
 // Custom functions that are to be wrapped from lua.
 // -------------------------------------------------
-const ter_t &get_terrain_type(int id)
-{
-    return ter_id( id ).obj();
-}
-
 static calendar &get_calendar_turn_wrapper() {
     return calendar::turn;
 }
