355fc9a808362598d2b022ec06a4461af589fb6c
BevapDin <Tho_Ki@gmx.de>
2018-07-30 11:35:40 +0200

    Move implementation of use_function into "iuse.cpp"
    The class is declared in the matching header "iuse.h", so it should be in the matching cpp file.

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -7711,3 +7711,41 @@ int iuse::disassemble( player *p, item *it, bool, const tripoint & )
     p->disassemble( *it, pos, false, false );
     return 0;
 }
+
+use_function::use_function( const use_function &other ) : actor( other.actor ? other.actor->clone()
+            : nullptr )
+{
+}
+
+use_function &use_function::operator=( iuse_actor *const f )
+{
+    return operator=( use_function( f ) );
+}
+
+use_function &use_function::operator=( const use_function &other )
+{
+    actor.reset( other.actor ? other.actor->clone() : nullptr );
+    return *this;
+}
+
+void use_function::dump_info( const item &it, std::vector<iteminfo> &dump ) const
+{
+    if( actor ) {
+        actor->info( it, dump );
+    }
+}
+
+ret_val<bool> use_function::can_call( const player &p, const item &it, const bool active,
+                                      const tripoint &pos ) const
+{
+    if( !actor ) {
+        return ret_val<bool>::make_failure( _( "You can't do anything interesting with your %s." ),
+                                            it.tname() );
+    }
+    return actor->can_use( p, it, active, pos );
+}
+
+long use_function::call( player &p, item &it, const bool active, const tripoint &pos ) const
+{
+    return actor->use( p, it, active, pos );
+}
