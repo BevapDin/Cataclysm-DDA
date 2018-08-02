d3fe5f32c9e09df3564756d122138d7f177edce9
BevapDin <Tho_Ki@gmx.de>
2018-08-02 09:40:17 +0200

    lex: (KEEP) extract global functions
    This reverts commit bc29897c761e191dc13ea10359411eec1a2dd69c.

---
+++ lex/CppFreeFunction.cpp
@@ -0,0 +1,38 @@
+#include "CppFreeFunction.h"
+
+#include "Exporter.h"
+#include "Cursor.h"
+#include "Type.h"
+#include "exceptions.h"
+
+CppFreeFunction::CppFreeFunction( const Cursor &c, const size_t arg_count ) : CppCallable( c )
+{
+    while( arguments_.size() > arg_count ) {
+        arguments_.pop_back();
+    }
+}
+
+CppFreeFunction::~CppFreeFunction() = default;
+
+std::string CppFreeFunction::export_( Exporter &p ) const
+{
+    // @todo: add support for *some* operators
+    if( is_operator() ) {
+        if( operator_name().compare(0, 3, "\"\"_") == 0)  {
+            throw SkippedObjectError( "user defined literal" );
+        }
+        throw SkippedObjectError( "operator" );
+    }
+
+    std::string line;
+    line = line + "{ ";
+    const std::string lua_name = p.lua_name( full_name() );
+    line = line + "name = \"" + lua_name + "\", ";
+    line = line + "rval = " + result_type_as_lua_string( p ) + ", ";
+    if( lua_name != full_name().as_string() ) {
+        line = line + "cpp_name = \"" + full_name() + "\", ";
+    }
+    line = line + "args = " + export_arguments( p );
+    line = line + " }";
+    return line;
+}
