6e0b54a0634642493be5b84d44eb0b44dbd91a50
BevapDin <Tho_Ki@gmx.de>
2018-10-03 15:26:58 +0200

    Rename `vehicle::get_parts(flag, false, false)` to `vehicle::get_parts(flag)`
    The function returns the same:
    - the first `false` means ignore enabled status,
    - the second `false` means do not include broken parts.
    
    This is what `get_parts(flag)` already does.

--- src/vehicle_use.cpp
+++ src/vehicle_use.cpp
@@ -80,8 +80,8 @@ void vehicle::add_toggle_to_opts( std::vector<uimenu_entry> &options,
                                   const std::string &flag )
 {
     // fetch matching parts and abort early if none found
-    auto found = get_parts( flag, false, false );
-    if( found.empty() ) {
+    const auto found = get_parts( flag );
+    if( empty( found ) ) {
         return;
     }
 
@@ -104,8 +104,8 @@ void vehicle::add_toggle_to_opts( std::vector<uimenu_entry> &options,
     options.emplace_back( -1, allow, key, msg );
 
     actions.push_back( [ = ] {
-        for( vehicle_part *e : found )
-        {
+        for( const vpart_reference vp : found ) {
+            vehicle_part *const e = &vp.vehicle().parts[vp.part_index()];
             if( e->enabled != state ) {
                 add_msg( state ? _( "Turned on %s" ) : _( "Turned off %s." ), e->name().c_str() );
                 e->enabled = state;
