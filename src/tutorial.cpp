21f40bf471b2ddc56fc6e1b0a624301eb54a3c3d
BevapDin <Tho_Ki@gmx.de>
2018-10-28 10:43:30 +0100

    Make tutorial character wield the initial lighter
    As the character starts naked, they have no storage space for the lighter and would drop it upon the first turn anyway.

--- src/tutorial.cpp
+++ src/tutorial.cpp
@@ -63,7 +63,7 @@ bool tutorial_game::init()
     g->u.toggle_trait( trait_id( "QUICK" ) );
     item lighter( "lighter", 0 );
     lighter.invlet = 'e';
-    g->u.inv.add_item( lighter, true, false );
+    g->u.weapon = lighter;
     g->u.set_skill_level( skill_id( "gun" ), 5 );
     g->u.set_skill_level( skill_id( "melee" ), 5 );
     g->load_map( omt_to_sm_copy( tripoint( lx, ly, 0 ) ) );
