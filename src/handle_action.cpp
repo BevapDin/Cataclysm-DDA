7b89465423f6f89b759dba2b83f1993c09af20db
BevapDin <Tho_Ki@gmx.de>
2018-11-03 15:37:17 +0100

    Inline variable veh.
    Call `vehicle()` each time it's needed. This will make future changes simpler to follow.

--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -406,25 +406,24 @@ static void open()
     u.moves -= 100;
 
     if( const optional_vpart_position vp = m.veh_at( openp ) ) {
-        vehicle *const veh = &vp->vehicle();
-        int openable = veh->next_part_to_open( vp->part_index() );
+        int openable = vp->vehicle().next_part_to_open( vp->part_index() );
         if( openable >= 0 ) {
             const vehicle *player_veh = veh_pointer_or_null( m.veh_at( u.pos() ) );
-            bool outside = !player_veh || player_veh != veh;
+            bool outside = !player_veh || player_veh != &vp->vehicle();
             if( !outside ) {
-                veh->open( openable );
+                vp->vehicle().open( openable );
             } else {
                 // Outside means we check if there's anything in that tile outside-openable.
                 // If there is, we open everything on tile. This means opening a closed,
                 // curtained door from outside is possible, but it will magically open the
                 // curtains as well.
-                int outside_openable = veh->next_part_to_open( vp->part_index(), true );
+                int outside_openable = vp->vehicle().next_part_to_open( vp->part_index(), true );
                 if( outside_openable == -1 ) {
-                    const std::string name = veh->part_info( openable ).name();
+                    const std::string name = vp->vehicle().part_info( openable ).name();
                     add_msg( m_info, _( "That %s can only opened from the inside." ), name.c_str() );
                     u.moves += 100;
                 } else {
-                    veh->open_all_at( openable );
+                    vp->vehicle().open_all_at( openable );
                 }
             }
         } else {
