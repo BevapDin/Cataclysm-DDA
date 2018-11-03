91ad608741f76129ca70559d7b6b6b3ba8bf5a5c
BevapDin <Tho_Ki@gmx.de>
2018-11-03 17:23:50 +0100

    Change vehicle::part_displayed_at to take point instead of x,y

--- src/veh_interact.cpp
+++ src/veh_interact.cpp
@@ -1726,7 +1726,7 @@ int veh_interact::part_at( int dx, int dy )
 {
     int vdx = -ddx - dy;
     int vdy = dx - ddy;
-    return veh->part_displayed_at( vdx, vdy );
+    return veh->part_displayed_at( point( vdx, vdy ) );
 }
 
 /**
