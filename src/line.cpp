6cbf35ab3f727bb3b331e937f595dfd15d0d1baa
BevapDin <Tho_Ki@gmx.de>
2019-05-25 13:17:46 +0200

    Fix calculation of dot product
    The z value of the first vector was not used at all, which is wrong.

--- src/line.cpp
+++ src/line.cpp
@@ -582,7 +582,7 @@ float rl_vec2d::dot_product( const rl_vec2d &v ) const
 
 float rl_vec3d::dot_product( const rl_vec3d &v ) const
 {
-    return x * v.x + y * v.y + y * v.z;
+    return x * v.x + y * v.y + z * v.z;
 }
 
 bool rl_vec2d::is_null() const
