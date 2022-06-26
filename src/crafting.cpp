030f723b83980d2ab64d7c904e8f3bb5e0828785
BevapDin <tho_ki@gmx.de>
2022-06-26 13:09:41 +0200

    Show kalories of crafting components more often.

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -1744,7 +1744,7 @@ comp_selection<item_comp> Character::select_item_component( const std::vector<it
                     break;
             }
 
-            if( is_food && ingredient.is_food() ) {
+            if( is_food || ingredient.is_food() ) {
                 text += kcal_values.first == kcal_values.second ? _( " %d kcal" ) : _( " %d-%d kcal" );
                 if( ingredient.has_flag( flag_RAW ) && remove_raw ) {
                     //Multiplier for RAW food digestion
